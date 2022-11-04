0737db4da435db838ba5168cb9a74f63ee41880b
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-11-04 16:31:19 +0000

    Extract raw data to csv

--- extract.py
+++ extract.py
@@ -1,19 +1,30 @@
+import itertools
+import pandas as pd
+
 from ipl_web_scrape.driver import setup_driver
 from ipl_web_scrape.extract_actions import get_links, get_batting_scorecard
 from ipl_web_scrape.base_page import MatchSchedulePage
+from ipl_web_scrape.batting_table import BattingTable
 
-# Setup chrome webdriver for match page
-browser = setup_driver(MatchSchedulePage())
 
-# Extract links
-locator = "a[class='ds-no-tap-higlight']"
-link_text = "indian-premier-league"
-links = get_links(browser, locator, link_text)
+def extract():
+    # Setup chrome webdriver for match page
+    browser = setup_driver(MatchSchedulePage())
 
-# for link in links
-batting_score_card = get_batting_scorecard(links[0])
+    # Extract links
+    locator = "a[class='ds-no-tap-higlight']"
+    link_text = "indian-premier-league"
+    links = get_links(browser, locator, link_text)
 
-print(batting_score_card[0])
-print(batting_score_card[1])
+    list_tables = []
+    for link in links:
+        list_tables.append(get_batting_scorecard(link))
 
-browser.quit()
+    flat_list_tables = list(itertools.chain(*list_tables))
+    batting_scores = pd.concat(flat_list_tables)
+
+    browser.quit()
+
+    print(batting_scores.head())
+
+    batting_scores.to_csv("./data/batting_scores.csv")
