9bac55ff306924a84c0d0dd65b90fe155cb2dcb1
victoria.beardow1 <victoria.beardow1@nhs.net>
2022-11-04 17:45:32 +0000

    Minor changes to extract

--- extract.py
+++ extract.py
@@ -1,10 +1,12 @@
 import itertools
 import pandas as pd
 
-from ipl_web_scrape.driver import setup_driver
-from ipl_web_scrape.extract_actions import get_links, get_batting_scorecard
+from ipl_web_scrape.extract_actions import (
+    setup_driver,
+    get_links,
+    get_batting_scorecard,
+)
 from ipl_web_scrape.base_page import MatchSchedulePage
-from ipl_web_scrape.batting_table import BattingTable
 
 
 def extract():
@@ -25,6 +27,7 @@ def extract():
 
     browser.quit()
 
-    print(batting_scores.head())
-
     batting_scores.to_csv("./data/batting_scores.csv")
+
+
+extract()
